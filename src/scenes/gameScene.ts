3db40bba37553f87fbcb274b71c6d95890f439da
essentialskills <jamiew@essentialskills.net>
2026-07-09 08:14:58 -0400

    clean up

--- src/scenes/gameScene.ts
+++ src/scenes/gameScene.ts
@@ -1,8 +1,4 @@
-import {
-    ObjectGroup,
-    StandardScene,
-    BaseController
-} from "@essentialskills/gameenginets";
+import { StandardScene, BaseController } from "@essentialskills/gameenginets";
 
 export class GameScene extends StandardScene {
     private readonly sceneFile = "scenes/levels/game/scene";
@@ -14,9 +10,6 @@ export class GameScene extends StandardScene {
     public sceneReady(): void {
         BaseController.debugMode = true;
         this.initSceneFromJSON(this.sceneFile);
-
-        // const player = this.instantiateGameObject(this.entities.map.Player, "player");
-        // this.objectGroups.addToGroups(player, ["gameObjects", ObjectGroup.INPUT]);
 
         const roundController = this.instantiateGameObject(
             this.entities.map.RoundController,
